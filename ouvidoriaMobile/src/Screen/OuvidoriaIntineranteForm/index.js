09231a789edef96c967e94d3dc42e46e27a2ab73
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-10-01 16:19:47 -0300

    Creation of the form structure

---
+++ ouvidoriaMobile/src/Screen/OuvidoriaIntineranteForm/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function OuvidoriaIntineranteForm() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default OuvidoriaIntineranteForm;
---
+++ ouvidoriaMobile/src/Screen/RomariaForm/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function RomariaForm() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default RomariaForm;
