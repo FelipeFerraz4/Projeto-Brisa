9126b867caab961e506bdb60861e3f9a78d293fc
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-10-11 08:44:31 -0300

    tela do servidor

--- ouvidoriaMobile/src/pages/FormularioHome/index.tsx
+++ ouvidoriaMobile/src/pages/FormularioHome/index.tsx
@@ -1,20 +1,44 @@
 import React, {useState} from 'react';
-import {Text, TextInput, View} from 'react-native';
-import styleComponent from './style';
-import person from '../../assets/icon/person.png';
+import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import styleServidor from './style';
+import Person from '../../assets/icon/person.png';
+import Logo from '../../assets/logo/logo.png';
+import arrow from '../../assets/icon/arrow_black.png';
 
 function FormularioHome() {
   const [Servidor, SetServidor] = useState('');
 
   return (
-    <View style={styleComponent.conteiner}>
-      <Text>Nome do servidor</Text>
-      <TextInput
-        style={styleComponent.textInput}
-        onChangeText={nome => {
-          SetServidor(nome);
-        }}
-      />
+    <View style={styleServidor.conteiner}>
+      <View style={styleServidor.campoHeader}>
+        <TouchableOpacity>
+          <Image source={arrow} style={styleServidor.iconVoltar} />
+        </TouchableOpacity>
+      </View>
+      <View style={styleServidor.campoLogo}>
+        <Image source={Logo} />
+      </View>
+      <View style={styleServidor.campoForm}>
+        <Text style={styleServidor.titulo}>Nome do servidor</Text>
+        <View style={styleServidor.textInput}>
+          <Image
+            source={Person}
+            style={styleServidor.textInputImage}
+            resizeMode="contain"
+          />
+          <TextInput
+            value={Servidor}
+            onChangeText={nome => {
+              SetServidor(nome);
+            }}
+          />
+        </View>
+      </View>
+      <View style={styleServidor.campoBotao}>
+        <TouchableOpacity style={styleServidor.botao}>
+          <Text style={styleServidor.botaoTexto}>Iniciar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- ouvidoriaMobile/src/pages/FormularioHome/style.tsx
+++ ouvidoriaMobile/src/pages/FormularioHome/style.tsx
@@ -1,21 +1,66 @@
 import {StyleSheet} from 'react-native';
 
-const styleComponent = StyleSheet.create({
+const styleServidor = StyleSheet.create({
   conteiner: {
-    marginHorizontal: 10,
-    marginVertical: 10,
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  campoHeader: {
+    width: '90%',
+    height: '6%',
+    marginHorizontal: '1%',
+    marginVertical: '1%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  iconVoltar: {
+    width: 35,
+  },
+  campoLogo: {
+    marginHorizontal: '2%',
+  },
+  campoForm: {
+    marginVertical: '15%',
+    width: '90%',
+    marginHorizontal: '2%',
+  },
+  campoBotao: {
+    marginHorizontal: '2%',
+    width: '90%',
+  },
+  titulo: {
+    fontSize: 20,
+    lineHeight: 30,
+    marginVertical: '2%',
+    fontWeight: 'bold',
+  },
+  textInput: {
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: '#2778C3',
+    flexDirection: 'row',
+  },
+  textInputImage: {
+    margin: 1,
+    width: '8%',
+  },
+  botao: {
+    backgroundColor: '#2778C3',
+    height: '25%',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  textInput: {
-    margin: 12,
-    width: '90%',
-    borderWidth: 3,
-    borderRadius: 10,
-    borderColor: '#2778C3',
+  botaoTexto: {
+    color: '#FFF',
+    marginHorizontal: 2,
+    marginVertical: 2,
+    fontSize: 20,
+    lineHeight: 30,
   },
 });
 
-export default styleComponent;
+export default styleServidor;
