7bc4397e469a62b8f7d47241bdf6a1a2b385a2ef
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-10-18 09:17:52 -0300

    form php

---
+++ Arquivo teste php/FelipeTeste/src/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+
+<!doctype html>
+<html lang="en">
+<header>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Forms</title>
+</header>
+<body>
+    <form>
+        
+        <input type="submit" value="Enviar" />
+    </form>
+</body>
+</html>
