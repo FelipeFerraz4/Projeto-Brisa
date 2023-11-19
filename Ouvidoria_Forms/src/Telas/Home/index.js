c2fd31e399c9179b80f64beaeb9b33fb222b0677
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-11-19 07:32:26 -0300

    instalando a biblioteca de icons

--- Ouvidoria_Forms/src/Telas/Home/index.js
+++ Ouvidoria_Forms/src/Telas/Home/index.js
@@ -3,10 +3,9 @@ import {View, Text, ScrollView, TouchableOpacity, Image} from 'react-native';
 import {CarregarRomaria, CarregarOuvidoria} from '../../serviços/CarregarForms';
 import Login from '../../assets/icon/person_circle_black.png';
 import Logo from '../../assets/logo/logo.png';
-// import OuvidoriaIntinerante from '../../assets/imagem/Ouvidoria_Intinerante.png';
-// import PadreCicero from '../../assets/imagem/Padre_Cícero2.png';
 import styleHome from './style';
 import {useNavigation} from '@react-navigation/native';
+import Header from '../../components/Header';
 
 function Home() {
   const navigation = useNavigation();
@@ -17,9 +16,6 @@ function Home() {
     navigation.navigate('Login');
   }
 
-  function handlePress() {
-    navigation.navigate('Upload');
-  }
   function formularioPress(id) {
     console.log(id);
     navigation.navigate('FormularioHome', {idForm: id});
@@ -29,15 +25,7 @@ function Home() {
     <View style={styleHome.app}>
       <ScrollView>
         <View style={styleHome.conteiner}>
-          <View style={styleHome.campoHeader}>
-            <Text style={styleHome.headerTexto}>Ouvidoria</Text>
-            <TouchableOpacity
-              onPress={() => {
-                LoginPage();
-              }}>
-              <Image source={Login} style={styleHome.icon} />
-            </TouchableOpacity>
-          </View>
+          <Header />
           <Image source={Logo} style={styleHome.logo} />
           <View style={styleHome.main}>
             <View>
@@ -49,13 +37,6 @@ function Home() {
                 onPress={() => {
                   formularioPress(dadoOuvidoria.id);
                 }}>
-                {/* <View style={styleHome.campoImage}>
-                  <Image
-                    source={OuvidoriaIntinerante}
-                    resizeMode="cover"
-                    style={styleHome.imagem}
-                  />
-                </View> */}
                 <View>
                   <Text style={styleHome.titulo}>{dadoOuvidoria.titulo}</Text>
                 </View>
@@ -65,27 +46,11 @@ function Home() {
                 onPress={() => {
                   formularioPress(dadoRomaria.id);
                 }}>
-                {/* <View style={styleHome.campoImage}>
-                  <Image
-                    source={PadreCicero}
-                    resizeMode="stretch"
-                    style={styleHome.imagem}
-                  />
-                </View> */}
                 <View>
                   <Text style={styleHome.titulo}>{dadoRomaria.titulo}</Text>
                 </View>
               </TouchableOpacity>
             </View>
-          </View>
-          <View style={styleHome.campoBotao}>
-            <TouchableOpacity
-              style={styleHome.botao}
-              onPress={() => {
-                handlePress();
-              }}>
-              <Text style={styleHome.botaoTexto}>Enviar Formulários</Text>
-            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
